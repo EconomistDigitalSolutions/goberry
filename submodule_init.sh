996dd800a397a675c8d95295eb2fe5eb2837ebaf
buddhamagnet <buddhamagnet@gmail.com>
2015-11-27 16:26:23 +0000

    add submodule init script

---
+++ submodule_init.sh
@@ -0,0 +1,12 @@
+rm -rf vendor/*
+git submodule add https://github.com/alecthomas/gobundle vendor/github.com/alecthomas/gobundle
+git submodule add https://github.com/buddhamagnet/goconsul vendor/github.com/buddhamagnet/goconsul
+git submodule add https://github.com/buddhamagnet/raml vendor/github.com/buddhamagnet/raml
+git submodule add https://github.com/buddhamagnet/yaml vendor/github.com/buddhamagnet/yaml
+git submodule add https://github.com/gorilla/context vendor/github.com/gorilla/context
+git submodule add https://github.com/gorilla/mux vendor/github.com/gorilla/mux
+git submodule add https://gopkg.in/logfmt.v0 vendor/gopkg.in/logfmt.v0
+git submodule add https://gopkg.in/stack.v1 vendor/gopkg.in/stack.v1
+git submodule add https://github.com/go-kit/kit vendor/github.com/go-kit/kit
+git submodule add https://github.com/EconomistDigitalSolutions/ramlapi vendor/github.com/EconomistDigitalSolutions/ramlapi
+git submodule add https://github.com/EconomistDigitalSolutions/watchman vendor/github.com/EconomistDigitalSolutions/watchman
